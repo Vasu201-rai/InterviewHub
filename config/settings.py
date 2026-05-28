a2995ffbd82ad48575ca1ed7b64e68c295b45ee4
Vasu Rai <rai.vasu2003@gmail.com>
2026-05-28 23:54:09 +0530

    fixed database imports

--- config/settings.py
+++ config/settings.py
@@ -67,14 +67,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #    }
 #}
 
-import os
 import dj_database_url
+import os
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3')
-    
-    
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
