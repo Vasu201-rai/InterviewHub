702df61db917bee439d73bf28b5df18444492bdf
Vasu Rai <rai.vasu2003@gmail.com>
2026-05-28 15:57:58 +0530

    postgres connected

--- config/settings.py
+++ config/settings.py
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 #  https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-   }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#   }
+#}
 
 #DATABASES = {
 #   'default': {
@@ -93,6 +93,13 @@ DATABASES = {
 #    }
 #}
 
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
