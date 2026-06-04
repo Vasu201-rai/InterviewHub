a2dcef4cad007eff2571b3d0242e29b0b59b9034
Vasu Rai <rai.vasu2003@gmail.com>
2026-06-04 20:22:58 +0530

    add login error message

--- portal/views.py
+++ portal/views.py
@@ -36,9 +36,7 @@ def signup_view(request):
     return render(request, 'signup.html')
 
 def login_view(request):
-
     if request.method == 'POST':
-
         username = request.POST.get('username')
         password = request.POST.get('password')
 
@@ -48,13 +46,15 @@ def login_view(request):
             password=password
         )
 
-        print(user)
+        print("USER:", user)
 
         if user is not None:
             login(request, user)
             return redirect('/dashboard/')
+        else:
+            messages.error(request, 'Invalid username or password!')
 
-    return render(request, 'login.html')
+    return render(request, 'login.html', {'messages': messages.get_messages(request)})
 
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
