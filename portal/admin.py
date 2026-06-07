314628d2b62ab9ebf2cc4a12667964225936de35
Vasu Rai <rai.vasu2003@gmail.com>
2026-06-07 12:47:44 +0530

    add bulk import export feature

--- portal/admin.py
+++ portal/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 from .models import Question
 
-admin.site.register(Question)
+class QuestionResource(resources.ModelResource):
+    class Meta:
+        model = Question
+        fields = ('id', 'title', 'answer', 'category', 'user')
+
+class QuestionAdmin(ImportExportModelAdmin):
+    resource_class = QuestionResource
+
+admin.site.register(Question, QuestionAdmin)
