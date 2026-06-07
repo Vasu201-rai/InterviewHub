0ee7a24ef5146ef1454ab61c3596ab92f932f310
Vasu Rai <rai.vasu2003@gmail.com>
2026-06-07 13:48:15 +0530

    fix import user auto assign

--- portal/admin.py
+++ portal/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
+from import_export.fields import Field
+from import_export.widgets import ForeignKeyWidget
+from django.contrib.auth.models import User
 from .models import Question
 
 class QuestionResource(resources.ModelResource):
     class Meta:
         model = Question
-        fields = ('id', 'title', 'answer', 'category', 'user')
+        fields = ('id', 'title', 'answer', 'category')
+        exclude = ('user',)
+
+    def before_import_row(self, row, **kwargs):
+        row['user'] = User.objects.filter(is_superuser=True).first().id
 
 class QuestionAdmin(ImportExportModelAdmin):
     resource_class = QuestionResource
